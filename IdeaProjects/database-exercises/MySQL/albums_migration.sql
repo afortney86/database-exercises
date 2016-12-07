0bc66dffbbdc7eeeeca1aa817f3d6572b475ed5b
Anthony Fortney <afortney86@gmail.com>
2016-12-07 13:07:48 -0600

    changed decimal to 18,2

--- IdeaProjects/database-exercises/MySQL/albums_migration.sql
+++ IdeaProjects/database-exercises/MySQL/albums_migration.sql
@@ -6,7 +6,7 @@ id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 artist VARCHAR (50) NOT NULL,
 album_name VARCHAR (100) NOT NULL,
 release_date YEAR,
-sales_in_millions DECIMAL(8,2) NOT NULL,
+sales_in_millions DECIMAL(18,2) NOT NULL,
 genre VARCHAR (50) NOT NULL,
 PRIMARY KEY (id)
 );
