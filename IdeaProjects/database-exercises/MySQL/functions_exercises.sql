c7665aa24d742605ea3feeca5ea692b0824d9579
Anthony Fortney <afortney86@gmail.com>
2016-12-08 14:55:30 -0600

    Add a GROUP BY clause to your query for last names with 'q' and not 'qu' to find the distinct combination of first and last names. You will find there were some duplicate first and last name pairs in your previous results. Add a count() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.

--- IdeaProjects/database-exercises/MySQL/functions_exercises.sql
+++ IdeaProjects/database-exercises/MySQL/functions_exercises.sql
@@ -1,9 +1,9 @@
 -- Update your query for 'Irena', 'Vidya', or 'Maya'.
 -- Use count(*) and GROUP BY to find the number of employees for each gender with those names.
 
-SELECT count(*)
+SELECT count(*) as 'count'
 FROM employees
-WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender;
 
 
@@ -11,32 +11,40 @@ GROUP BY gender;
 -- Update your queries for employees whose names start and end with 'E'.
 -- Use concat() to combine their first and last name together as a single column in your results.
 
-SELECT concat(first_name, last_name)
+SELECT concat(first_name, '' ,last_name)
 FROM employees
 where last_name
       LIKE 'e%' AND last_name LIKE '%e'
 ORDER BY emp_no DESC ;
 
 
--- Change the query for employees hired in the 90s and born on Christmas such that the first
--- result is the oldest employee who was hired last. It should be Khun Bernini.
+-- For your query of employees born on Christmas and hired in the 90s,
+-- use datediff() to find how many days they have been working at the company
+-- (Hint: You will also need to use now() or curdate())
 
 
--- Employees hired in the 90's and born on christmas
-SELECT *
+SELECT *,
+  datediff(curdate(), hire_date) / 365 as 'years working'
 FROM employees
 WHERE hire_date
       BETWEEN '1990-1-1'
       AND '1999-12-31'
       AND birth_date LIKE '%12-25'
-ORDER BY birth_date ASC, hire_date DESC ;
+ORDER BY `Days working` DESC ;
 
 
+-- Add a GROUP BY clause to your query for last names with 'q' and not 'qu' to find the
+-- distinct combination of first and last names. You will find there were some duplicate
+-- first and last name pairs in your previous results.
+-- Add a count() to your results and use ORDER BY to make it easier to find
+-- employees whose unusual name is shared with others.
 
--- employees born on christmas
-SELECT *
-  AS "Employees born on christmas"
-from employees
-WHERE birth_date
-      LIKE '%12-25';
+SELECT count(*) AS 'repeats',
+  first_name,
+  last_name
+FROM employees
+WHERE last_name
+      like '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY first_name, last_name
+ORDER BY repeats DESC ;
 
