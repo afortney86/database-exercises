908be10941c07df88b0f8d25e5534a9b4a7f000b
Anthony Fortney <afortney86@gmail.com>
2016-12-08 12:43:06 -0600

    Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY

--- IdeaProjects/database-exercises/MySQL/group_by_exercise.sql
+++ IdeaProjects/database-exercises/MySQL/group_by_exercise.sql
@@ -3,3 +3,12 @@
 SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC;
+
+-- Find your query for employees whose last names start and end with 'E'.
+-- Update the query find just the unique last names that start and end with 'E' using GROUP BY
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name
+      LIKE 'e%' AND last_name LIKE '%e'
+GROUP BY last_name ASC ;
