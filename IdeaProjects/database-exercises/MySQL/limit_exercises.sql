7d73528472e96020ff386a091930daac4ffa317e
Anthony Fortney <afortney86@gmail.com>
2016-12-08 13:31:36 -0600

    Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees.

--- IdeaProjects/database-exercises/MySQL/limit_exercises.sql
+++ IdeaProjects/database-exercises/MySQL/limit_exercises.sql
@@ -6,3 +6,13 @@ WHERE last_name
 ORDER BY last_name DESC
 LIMIT 10;
 
+-- Find your query for employees born on Christmas and hired in the 90s
+-- from order_by_exercises.sql. Update it to find just the first 5 employees.
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date
+      BETWEEN '1990-1-1'
+      AND '1999-12-31' AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
