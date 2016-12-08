502adcf3b0c7bcef8bceef46d36c76b540a34645
Anthony Fortney <afortney86@gmail.com>
2016-12-08 13:16:15 -0600

    Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY.

--- IdeaProjects/database-exercises/MySQL/group_by_exercise.sql
+++ IdeaProjects/database-exercises/MySQL/group_by_exercise.sql
@@ -7,8 +7,14 @@ ORDER BY title ASC;
 -- Find your query for employees whose last names start and end with 'E'.
 -- Update the query find just the unique last names that start and end with 'E' using GROUP BY
 
+SELECT first_name, last_name
+FROM employees
+WHERE first_name AND last_name
+      LIKE 'e%' AND first_name AND last_name LIKE '%e';
+
+-- Find the unique last names with a 'q' but not 'qu'.
+-- You may use either DISTINCT or GROUP BY. Your results should be:
+
 SELECT DISTINCT last_name
 FROM employees
-WHERE last_name
-      LIKE 'e%' AND last_name LIKE '%e'
-GROUP BY last_name ASC ;
+WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%';
